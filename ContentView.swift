0df67c9dd1e46c77c27db96511fee33f874585a8
iWinter <72822518+WinterBoii@users.noreply.github.com>
2022-12-01 14:34:25 +0100

    added question data

---
+++ ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Quift
+//
+//  Created by Filmon Mehari Gebrezghi on 2022-11-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var quizManager = QuizManager.shared
+    
+    var body: some View {
+        NavigationStack {
+            VStack(spacing: 40) {
+                VStack(spacing: 20) {
+                    Text("Quiz Game")
+                        .lilacTitle()
+                    
+                    Text("Are you ready to play?")
+                        .foregroundColor(Color("AccentColor"))
+                }
+                
+                NavigationLink {
+                    TriviaView()
+                        .environmentObject(quizManager)
+                } label: {
+                    PrimaryButton(text: "Lets Go!")
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .edgesIgnoringSafeArea(.all)
+            .background(quizManager.backgroundColor)
+        }
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
