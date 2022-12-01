593d703ea67994a14868b28768da32f53ae38287
iWinter <72822518+WinterBoii@users.noreply.github.com>
2022-12-01 14:36:50 +0100

    add models

---
+++ Model/Quiz.swift
@@ -0,0 +1,51 @@
+//
+//  Quiz.swift
+//  Quift
+//
+//  Created by Muhammad Amiin Obidhonyi on 2022-11-28.
+//
+
+import Foundation
+
+struct QuizData: Decodable {
+    var results: [Result]
+    
+    struct Result: Decodable, Identifiable {
+        var id: UUID {
+            UUID()
+        }
+        var category: String
+        var type: String
+        var difficulty: String
+        var question: String
+        var correctAnswer: String
+        var incorrectAnswers: [String]
+        
+        
+        var formattedQuestion: AttributedString {
+            //we need to wrap in do catch, due to attributedString can throw error
+            do {
+                return try AttributedString(markdown: question)
+            } catch {
+                print("Error setting formattedQuestion \(error)")
+                return ""
+            }
+        }
+        //we dont want to call correct and incorrect answer everytime, so we centrelize
+        var answers: [Answer] {
+            do {
+                let correct = [Answer(text: try AttributedString(markdown: correctAnswer), isCorrect: true)]
+                let incorrects = try incorrectAnswers.map { answer in
+                    Answer(text: try AttributedString(markdown: answer), isCorrect: false)
+                }
+                
+                let allAnswers = correct + incorrects
+                
+                return allAnswers.shuffled()
+            } catch {
+                print("Error setting answers: \(error)")
+                return []
+            }
+        }
+    }
+}
