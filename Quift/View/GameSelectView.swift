c96e0004652cc287e5005ab74d249ab8aca1fbcc
iWinter <72822518+WinterBoii@users.noreply.github.com>
2022-12-01 20:34:41 +0100

    v2 of selection done

--- Quift/View/GameSelectView.swift
+++ Quift/View/GameSelectView.swift
@@ -10,18 +10,68 @@ import SwiftUI
 struct GameSelectView: View {
     @StateObject var quizManager = QuizManager.shared
     
-    var difficulty = ["😌", "🧐", "🤯"]
+    var difficulties = ["😌", "🧐", "🤯"]
+    var categories: [String:Int] = [
+        "General Knowledge": 9,
+        "Film": 11,
+        "Video Games" : 15,
+        "Science & Nature": 17,
+        "Computers": 18,
+        "Mathematics": 19,
+        "Sports": 21,
+        "Geography": 22,
+        "Japanese Anime & Manga": 31,
+        "Cartoon & Animations": 32
+    ]
     @State var selectedDifficulty: String?
+    @State var selectedCategory: String?
     
     var body: some View {
         VStack {
             Spacer()
-            Text("Select")
+                .frame(height: 30)
+            Text("Select Options")
+                .font(.system(size: 50))
                 .lilacTitle()
+                .underline()
             Spacer()
             
+            Text("Category:")
+                .font(.largeTitle)
+                .lilacTitle()
+            
+            ScrollView(.horizontal) {
+                LazyHStack(spacing: 55) {
+                    ForEach(categories.sorted(by: >), id: \.key) { key, value in
+                        Button {
+                            if selectedCategory == key {
+                                selectedCategory = nil
+                            } else {
+                                selectedCategory = key
+                            }
+                            print(key)
+                        } label: {
+                            Text("\(key)")
+                        }
+                        .padding()
+                        .foregroundColor(selectedCategory == key ? Color.white : Color("AccentColor"))
+                        .background(selectedCategory == key ? Color("AccentColor") : quizManager.backgroundColor)
+                        .cornerRadius(25)
+                    }
+                    .font(.system(size: 25))
+                    
+                }
+            }
+            .frame(width: 330 ,height: 99)
+            
+            Spacer()
+            
+            Text("Difficulty:")
+                .font(.largeTitle)
+                .lilacTitle()
+            
             HStack {
-                ForEach(difficulty, id: \.self) { index in
+                ForEach(difficulties, id: \.self) { index in
                     Button("\(index)") {
                         if selectedDifficulty == index {
                             selectedDifficulty = nil
@@ -33,10 +83,22 @@ struct GameSelectView: View {
                     .font(.system(size: 50))
                     .padding()
                     .background(selectedDifficulty == index ? Color("AccentColor") : quizManager.backgroundColor)
+                    .cornerRadius(33)
+                    .padding(.bottom)
                 }
             }
-            Spacer()
             
+            NavigationLink {
+                TriviaView()
+            } label: {
+                PrimaryButton(text: "Start Quiz")
+                    .padding(.top)
+                    .font(.system(size: 33))
+                    .onSubmit {
+                        //quizManager.getOptions(id: selectedCategory.value, level: selectedDifficulty, nrOfQuestions: "")
+                    }
+            }
+            Spacer()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(quizManager.backgroundColor)
