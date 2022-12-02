b0a6057e1ab43e4a20761db52ebffd1dbea28be0
Filmon <112778762+nomliF@users.noreply.github.com>
2022-12-02 19:24:46 +0100

    Added quit view

---
+++ Quift/View/QuitGameView.swift
@@ -0,0 +1,47 @@
+//
+//  QuitGameView.swift
+//  Quift
+//
+//  Created by Filmon Mehari Gebrezghi on 2022-12-02.
+//
+
+import SwiftUI
+
+struct QuitGameView: View {
+    @StateObject var quizManager = QuizManager.shared
+    @Environment(\.dismiss) private var dismiss
+    @State private var showDismissWarning = false
+    
+    var body: some View {
+        NavigationStack{
+            HStack{
+                Text("quit")
+            }
+
+            .toolbar{
+                Button{
+                    showDismissWarning.toggle()
+                } label: {
+                    Label("Quit", systemImage: "xmark")
+                        .foregroundColor(Color("AccentColor"))
+                }
+                .alert("Dou want to quit?", isPresented: $showDismissWarning){
+                    Button(role: .destructive){
+                        dismiss()
+                    }label: {
+                        Text("Yes")
+                    }
+                }
+            }
+
+
+        }.background(quizManager.backgroundColor)
+        
+        
+    }
+}
+    struct QuitGameView_Previews: PreviewProvider {
+        static var previews: some View {
+            QuitGameView()
+        }
+    }
